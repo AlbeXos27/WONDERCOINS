576b1df823b3e8362c257617b9dc009a283bf1f7
Alberto Contreras Puerto <105641564+AlbeXos21@users.noreply.github.com>
2025-08-11 19:13:57 +0200

    creacion jerarquia de cecas

--- web_numisdata/tpl/mints_hierarchy/js/mints_hierarchy.js
+++ web_numisdata/tpl/mints_hierarchy/js/mints_hierarchy.js
@@ -19,10 +19,80 @@ var mints_hierarchy =  {
 
 		self.rows_container	= options.rows_container
 
-		await this.get_data_mints(null)
+		const cecas_iniciales = await this.get_data_mints(21057)
 
-		return true
+		const arbol_completo = common.create_dom_element({
+			element_type : "div",
+			class_name	 : "arbol_completo",
+			parent		 : self.rows_container
+		})
+
+		this.generate_data_mint(21057,arbol_completo,0);
+
+		return true;
+
 	},//end set_up
+
+	generate_data_mint: async function(index,nodo_padre,tab) {
+
+		const cecas = await this.get_data_mints(index);
+
+		for (let index = 0; index < cecas.total; index++) {
+	
+			const info_node = common.create_dom_element({
+				element_type	: "div",
+				class_name		: "container_cecas",
+				text_content	: cecas.result[index].term,
+				parent			: nodo_padre
+			})
+
+			info_node.style.paddingLeft = `${tab}em`;
+
+
+			if(cecas.result[index].children)
+			{
+				const button_display = common.create_dom_element({
+					element_type	: "img",
+					class_name		: `button_display`,
+					src				: "tpl/assets/images/arrow-right.svg",
+					parent			: info_node
+				})
+				button_display.classList.add(`button_display_${cecas.result[index].section_id}`)
+			}
+
+			let estado_mostrar_monedas = false
+			let hijos_creados_monedas = false
+			document.addEventListener("mousedown", (e) => {
+
+			const button = e.target.closest(`.button_display_${cecas.result[index].section_id}`);
+			const hijos = Array.from(info_node.children).slice(1);
+
+				if (estado_mostrar_monedas) {
+
+					button.style.transform = "rotate(0deg) translateY(20%) translateX(30%)";
+						hijos.forEach(hijo => {
+							hijo.style.display = 'none'; 
+						});
+				} else {
+
+					button.style.transform = "rotate(90deg) translateX(30%) translateY(-20%)";
+						hijos.forEach(hijo => {
+							hijo.style.display = 'block'; 
+						});
+					if(!hijos_creados_monedas){
+
+						this.generate_data_mint(cecas.result[index].section_id,info_node,1.5)
+						hijos_creados_monedas = !hijos_creados_monedas;
+					}
+					
+				}
+
+				estado_mostrar_monedas = !estado_mostrar_monedas;
+
+			});
+
+		}
+	},
 
 	get_data_mints: async function(id) {
 			try {
@@ -31,11 +101,11 @@ var mints_hierarchy =  {
 						dedalo_get: 'records',
 						table: 'catalog',
 						ar_fields: ["*"],
-						sql_filter: id == null ? "section_id = 21057" : `parent LIKE "%${id}%"`,
+						sql_filter: `parent LIKE '%"${id}"%'`,
 						limit: 0,
 						count: true,
 						offset: 0,
-						order: '',
+						order: 'section_id',
 						process_result: null
 					}
 				});
@@ -45,9 +115,6 @@ var mints_hierarchy =  {
 				return null;
 			}
 	},
-
-
-
 
 
 }
